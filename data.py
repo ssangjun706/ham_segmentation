1f8f573c01d0ea85e4080871df148a76edddceee
ssangjun706 <ssangjun706@naver.com>
2024-07-04 16:31:49 +0900

    [Modify] data.py

--- data.py
+++ data.py
@@ -1,7 +1,11 @@
+import os
 from torch.utils.data import Dataset
 
-class HAMDataset(Dataset):
-    def __init__(self):
+
+class HAM10000(Dataset):
+    def __init__(self, dir):
+        self.files = os.listdir(dir)
+        print(len(self.files))
         pass
 
     def __len__(self):
